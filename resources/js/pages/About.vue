84427e9d56625df729956bd52f5dff8e8fcdecf6
Annisa Maulidina <annisamaulidina12@gmail.com>
2025-08-22 00:27:17 +0700

    feat: about page & responsive navbar

--- resources/js/pages/About.vue
+++ resources/js/pages/About.vue
@@ -2,77 +2,117 @@
 import { Head } from '@inertiajs/vue3'
 import Navbar from '../components/Navbar.vue';
 import Footer from '../components/Footer.vue';
-import fotoParlemen from '/public/images/home_parlemen.png';
 </script>
 <template>
     <Head title="DPM FILKOM UB - Tentang"/>
-    <Navbar/>
+    <Navbar class=""/>
     <div class="about">
         <!-- Header -->
-        <header class="relative py-20 px-4 sm:px-6 lg:px-8 flex justify-center items-center text-center
-                bg-[url('/images/home_parlemen.png')] bg-center bg-cover bg-no-repeat h-200">
-            <div class="absolute inset-0 bg-neutral-950/50"></div>
+        <header class="relative py-20 px-4 sm:px-6 md:px-16 lg:px-20 flex justify-center items-center text-center
+                bg-[url('/images/home_parlemen.png')] bg-center bg-cover bg-no-repeat h-screen">
+            <div class="absolute inset-0 bg-gradient-to-b from-black/90 to-black/0"></div>
             <div class="relative z-10 text-center text-white">
-                <p class="bg-[#B48B52] mx-auto w-[150px] px-2 py-2 rounded-4xl font-medium text-xl mb-2">Tentang</p>
-                <h1 class="text-6xl font-bold mb-2">DPM FILKOM UNIVERSITAS BRAWIJAYA</h1>
-                <p class="text-xl mb-5">Parlemen Katha Mantrana 2025</p>
-                <h2 class="text-2xl font-semibold">Tak Kenal Maka Kenalan Yuk!</h2>
+                <p class="bg-[#B48B52] mx-auto w-[150px] px-2 py-2 rounded-4xl text-lg mb-2">Tentang</p>
+                <h1 class="text-5xl font-medium mb-2">DPM FILKOM UNIVERSITAS BRAWIJAYA</h1>
+                <h2 class="text-2xl">Tak Kenal Maka Kenalan Yuk!</h2>
+            </div>
+            <div class="absolute flex justify-center -bottom-12 bg-[#5B352C] h-24 items-center rounded-full">
+                <p class="text-2xl text-white py-6 px-96">Parlemen Katha Mantrana 2025</p>
             </div>
         </header>
         <!-- Visi -->
-        <section class="my-20 mx-auto max-w-[80%] px-4 sm:px-6 lg:px-8 flex flex-col items-center text-center">
-            <h2>Visi</h2>
-            <p>Mewujudkan DPM FILKOM Sebagai lembaga yang menjalankan fungsi aspirasi, legislasi, dan pengawasan dengan mengutamakan nilai integritas serta Keterbukaan untuk kemaslahatan KBM FILKOM.</p>
+        <section class="mt-40 mx-20 px-4 sm:px-6 md:px-16 lg:px-20 flex flex-col items-center justify-center">
+            <div class="gap-5 flex flex-col items-center px-10 py-5 text-center">
+                <h2 class="text-4xl text-[#5B352C] font-medium">Visi</h2>
+                <p class="text-lg text-gray-700">Mewujudkan DPM FILKOM Sebagai lembaga yang menjalankan fungsi aspirasi, legislasi, dan pengawasan dengan mengutamakan nilai integritas serta Keterbukaan untuk kemaslahatan KBM FILKOM.</p>
+            </div>
         </section>
         <!-- Misi -->
-        <section class="my-20 mx-auto max-w-[80%] px-4 sm:px-6 lg:px-8 flex flex-col justify-center items-center">
-            <h2>Misi</h2>
-            <div class="grid">
-                <div class="">
-                    <span>1</span>
-                    <p><strong>Proaktif dan Responsif</strong> dalam menindaklanjuti aspirasi KBMFILKOM.</p>
+        <section class="mt-24 mx-20 px-4 sm:px-6 md:px-16 lg:px-20 flex flex-col justify-center gap-6 items-center">
+            <h2 class="text-4xl text-[#5B352C] font-medium">Misi</h2>
+            <div class="grid gap-6 grid-cols-3">
+                <div class="p-10 h-64 gap-2.5 flex flex-col text-center bg-[#F8F4EC] rounded-xl text-gray-900">
+                    <h1 class="font-semibold text-xl">1</h1>
+                    <p class="text-xl"><strong>Proaktif dan Responsif</strong> dalam menindaklanjuti aspirasi KBMFILKOM.</p>
                 </div>
-                <div class="">
-                    <span>2</span>
-                    <p><strong>Meningkatkan partisipasi publik</strong> dengan mendorong penetapan keputusan bersifat kolektif dan inklusif.</p>
+                <div class="p-10 h-64 gap-2.5 flex flex-col text-center bg-[#5B352C] rounded-xl text-white">
+                    <h1 class="font-semibold text-xl">2</h1>
+                    <p class="text-xl"><strong>Meningkatkan partisipasi publik</strong> dengan mendorong penetapan keputusan bersifat kolektif dan inklusif.</p>
                 </div>
-                <div class="">
-                    <span>3</span>
-                    <p><strong>Mengutamakan akuntabilitas</strong> guna menciptakan pengawasan yang optimal.</p>
+                <div class="p-10 h-64 gap-2.5 flex flex-col text-center bg-[#F8F4EC] rounded-xl text-gray-900">
+                    <h1 class="font-semibold text-xl">3</h1>
+                    <p class="text-xl"><strong>Mengutamakan akuntabilitas</strong> guna menciptakan pengawasan yang optimal.</p>
                 </div>
-                <div class="">
-                    <span>4</span>
-                    <p><strong>Mengoptimalkan transparasi</strong> dengan mendorong penetapan keputusan bersifat kolektif dan inklusif.</p>
+                <div class="p-10 h-64 gap-2.5 flex flex-col text-center bg-[#5B352C] rounded-xl text-white">
+                    <h1 class="font-semibold text-xl">4</h1>
+                    <p class="text-xl"><strong>Mengoptimalkan transparasi</strong> dengan mendorong penetapan keputusan bersifat kolektif dan inklusif.</p>
                 </div>
-                <div class="">
-                    <span>5</span>
-                    <p><strong>Mengedepankan kesetaraan</strong> untuk mewujudkan demokrasi yang bebas dan adil</p>
+                <div class="p-10 h-64 gap-2.5 flex flex-col text-center bg-[#F8F4EC] rounded-xl text-gray-900">
+                    <h1 class="font-semibold text-xl">5</h1>
+                    <p class="text-xl"><strong>Mengedepankan kesetaraan</strong> untuk mewujudkan demokrasi yang bebas dan adil</p>
                 </div>
-                <div class="">
-                    <span>6</span>
-                    <p><strong>Mendorong kolaborasi</strong> sebagai bentuk keharmonisan untuk mencapai tujuan bersama.</p>
+                <div class="p-10 h-64 gap-2.5 flex flex-col text-center bg-[#5B352C] rounded-xl text-white">
+                    <h1 class="font-semibold text-xl">6</h1>
+                    <p class="text-xl"><strong>Mendorong kolaborasi</strong> sebagai bentuk keharmonisan untuk mencapai tujuan bersama.</p>
                 </div>
             </div>
         </section>
         <!-- Nilai -->
-        <section class="my-20 mx-auto max-w-[80%] px-4 sm:px-6 lg:px-8 flex flex-col justify-around items-center">
-            <h2>Nilai</h2>
-            <div class="flex flex-row gap-4">
-                <p class="text-[#5B352C] p-2 rounded-sm outline-2 outline-[#5B352C]">Demokrasi</p>
-                <p class="text-[#5B352C] p-2 rounded-sm outline-2 outline-[#5B352C]">inklusif</p>
-                <p class="text-[#5B352C] p-2 rounded-sm outline-2 outline-[#5B352C]">Kebebasan</p>
-                <p class="text-[#5B352C] p-2 rounded-sm outline-2 outline-[#5B352C]">integritas</p>
-                <p class="text-[#5B352C] p-2 rounded-sm outline-2 outline-[#5B352C]">Transparasi</p>
-                <p class="text-[#5B352C] p-2 rounded-sm outline-2 outline-[#5B352C]">Akuntabilitas</p>
+        <section class="mt-24 mx-20 px-4 sm:px-6 md:px-16 lg:px-20 flex flex-col gap-6 justify-around items-center">
+            <h2 class="text-4xl text-[#5B352C] font-medium">Nilai</h2>
+            <div class="flex gap-6 justify-start">
+                <p class="text-[#5B352C] text-xl py-4 px-10 rounded-sm border-1 border-[#5B352C]">Demokrasi</p>
+                <p class="text-[#5B352C] text-xl py-4 px-10 rounded-sm border-1 border-[#5B352C]">Inklusif</p>
+                <p class="text-[#5B352C] text-xl py-4 px-10 rounded-sm border-1 border-[#5B352C]">Kebebasan</p>
+                <p class="text-[#5B352C] text-xl py-4 px-10 rounded-sm border-1 border-[#5B352C]">Integritas</p>
+                <p class="text-[#5B352C] text-xl py-4 px-10 rounded-sm border-1 border-[#5B352C]">Transparasi</p>
+                <p class="text-[#5B352C] text-xl py-4 px-10 rounded-sm border-1 border-[#5B352C]">Akuntabilitas</p>
             </div>
         </section>
         <!-- Logo -->
-        <section class="my-20 mx-auto max-w-[80%] px-4 sm:px-6 lg:px-8">
-            <h2></h2>
+        <section class="my-20 mx-20 px-4 sm:px-6 md:px-16 lg:px-20">
+            <h2 class="text-4xl text-[#5B352C] font-medium">LOGO DPM FILKOM UB</h2>
+            <div class="justify-center items-center flex flex-col mt-60 mb-80">
+                <div class="absolute ml-196 -mt-160 justify-center items-center flex flex-col gap-5">
+                    <img src="/public/images/logo-5-orang.png" alt="" class="h-20 ">
+                    <p class="text-center text-lg">Lorem ipsum</p>
+                </div>
+                <img src="/public/images/vector-1.png" alt="" class="h-50 absolute ml-72 -mt-[400px]">
+                <div class="absolute mr-208 -mt-96 justify-center items-center flex flex-col gap-5">
+                    <img src="/public/images/logo-5-orang.png" alt="" class="h-20 ">
+                    <p class="text-center text-lg">Lorem ipsum</p>
+                </div>
+                <img src="/public/images/vector-3.png" alt="" class="h-40 absolute mr-72 -mt-44">
+                <img src="/public/images/logo_dpmfilkomub.png" alt="" class="h-80 w-auto justify-center mx-auto">
+                <div class="absolute ml-180 -mb-120 justify-center items-center flex flex-col gap-5">
+                    <img src="/public/images/logo-buku-terbuka.png" alt="" class="h-20 ">
+                    <p class="text-center text-lg">Lorem ipsum</p>
+                </div>
+                <img src="/public/images/vector-2.png" alt="" class="h-40 absolute ml-72 -mb-80">
+                <div class="absolute mr-196 -mb-140 justify-center items-center flex flex-col gap-5">
+                    <img src="/public/images/logo_dpmfilkomub.png" alt="" class="h-20 ">
+                    <p class="text-center text-lg">Lorem ipsum</p>
+                </div>
+                <img src="/public/images/vector-4.png" alt="" class="h-56 absolute mr-56 -mb-72">
+            </div>
         </section>
         <!-- Warna -->
-        <section class="my-20 mx-auto max-w-[80%] px-4 sm:px-6 lg:px-8">
-            <h2></h2>
+        <section class="mt-28 mx-20 px-4 sm:px-6 md:px-16 lg:px-20 flex flex-col gap-10 justify-center items-center">
+            <div class="grid grid-cols-3 gap-72">
+                <div class="flex flex-col items-center">
+                    <div class="w-24 h-24 bg-[#5C342A] rounded-xl"></div>
+                    <p class="text-gray-900 text-lg mt-2">#5C342A</p>
+                </div>
+                <div class="flex flex-col items-center">
+                    <div class="w-24 h-24 bg-[#B48B52] rounded-xl"></div>
+                    <p class="text-gray-900 text-lg mt-2">#B48B52</p>
+                </div>
+                <div class="flex flex-col items-center">
+                    <div class="w-24 h-24 bg-[#F8F4EC] rounded-xl"></div>
+                    <p class="text-gray-900 text-lg mt-2">#F8F4EC</p>
+                </div>
+            </div>
+            <p class="text-center text-lg text-gray-900 mx-68">Warna emas dan coklat sebagai simbol kebijaksanaan, kematangan, dan kredibilitas dalam menjalankan tugas organisasi.</p>
         </section>
     </div>
     <Footer/>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -19,20 +19,20 @@ import logoMedkom from '/public/images/logo_medkom.png';
 
 <template>
     <Head title="DPM FILKOM UB - Beranda"/>
-    <Navbar/>
-    <div class="home">
+    <Navbar :default-white="true"/>
+    <div class="home mt-40">
         <!-- Header -->
-        <header class="header my-20 mx-auto max-w-[80%] px-4 sm:px-6 lg:px-8 flex flex-row justify-center items-center gap-10">
+        <header class="header my-20 mx-20 px-4 sm:px-6 md:px-16 lg:px-20 flex flex-row justify-center items-center gap-10">
             <div class="text-container max-w-1/2 gap-4">
-                <H2 class="text-[#B48B52] text-2xl font-semibold mb-2">DPM FILKOM UB - Katha Mantrana</H2>
-                <H1 class="text-[#5B352C] text-5xl font-bold mb-10">Bangkitkan Asa,<br>Gelorakan Suara!</H1>
-                <p class="text-lg text-neutral-700 font-medium max-w-fit container text-justify">Lembaga legislatif tingkat fakultas yang berperan sebagai penjembatan aspirasi mahasiswa,  bertanggung jawab atas pembentukan dan pengesahan hukum di tingkat mahasiswa, dan pengawas kinerja Badan Ekskutif Mahasiswa FILKOM UB.</p>
+                <H2 class="text-[#B48B52] text-2xl mb-2">DPM FILKOM UB - Katha Mantrana</H2>
+                <H1 class="text-[#5B352C] text-5xl font-semibold mb-10">Bangkitkan Asa,<br>Gelorakan Suara!</H1>
+                <p class="text-lg text-neutral-700 max-w-fit container text-justify">Lembaga legislatif tingkat fakultas yang berperan sebagai penjembatan aspirasi mahasiswa,  bertanggung jawab atas pembentukan dan pengesahan hukum di tingkat mahasiswa, dan pengawas kinerja Badan Ekskutif Mahasiswa FILKOM UB.</p>
             </div>
             <img :src="home_header" alt="Gedung GKM" class="w-1/2">
         </header>
         <!-- Program Kerja -->
-        <section class="proker my-20 mx-auto max-w-[80%] px-4 sm:px-6 lg:px-8">
-            <h2 class="mb-15 text-4xl text-[#5B352C] font-bold text-center">Program Kerja Unggulan</h2>
+        <section class="proker my-20 mx-20 px-4 sm:px-6 md:px-16 lg:px-20">
+            <h2 class="mb-15 text-4xl text-[#5B352C] font-semibold text-center">Program Kerja Unggulan</h2>
             <div class="proker-carousel flex flex-row gap-4 items-center justify-around">
                 <div class="proker-card text-neutral-900">
                     <img :src="prokerPA" alt="Program Kerja Portal Aspirasi">
@@ -44,7 +44,7 @@ import logoMedkom from '/public/images/logo_medkom.png';
                 </div>
                 <div class="proker-card text-neutral-900">
                     <img :src="prokerLA" alt="Program Kerja Legislative Academy">
-                    <h5 class="text-center text-xl font-normal mt-4">legislative Academy</h5>
+                    <h5 class="text-center text-xl font-normal mt-4">Legislative Academy</h5>
                 </div>
             </div>
         </section>
@@ -61,7 +61,7 @@ import logoMedkom from '/public/images/logo_medkom.png';
             </div>
         </section>
         <!-- Ucapan Selamat Datang -->
-        <section class="my-20 mx-auto max-w-[80%] px-4 sm:px-6 lg:px-8 flex flex-row gap-15 justify-center items-center">
+        <section class="my-20 mx-20 px-4 sm:px-6 md:px-16 lg:px-20 flex flex-row gap-15 justify-center items-center">
             <!-- Gambar dan Identitas -->
             <img :src="welcomePotrait" class="max-w-100 h-auto" alt="Koordinator DPM FILKOM 2025 Jildan Adam Zein">
             <!-- Teks Selamat datang -->
@@ -70,18 +70,18 @@ import logoMedkom from '/public/images/logo_medkom.png';
                 <h3 class="text-[#5B352C] text-4xl font-semibold mb-5">Laman Resmi DPM FILKOM UB</h3>
                 <p class="text-neutral-700 text-lg mb-10">Sebagai representasi suara mahasiswa, kami berkomitmen untuk menjadi jembatan aspirasi, pengawal kebijakan, dan mitra strategis dalam memajukan FILKOM. Mari bersama membangun lingkungan akademik yang inspiratif dan berprestasi.</p>
                 <!-- Ini masih belum nimpa gambar (mungkin bisa pake absolute atau semacamnya) -->
-                <div class="flex flex-row gap-8 justify-center items-center">
-                    <div class="card bg-white border-[#5B352C] border-r-4 border-b-4 p-2 rounded-[10px] w-100">
+                <div class="flex gap-8 items-center mt-16">
+                    <div class="absolute -left-52 card bg-white border-[#5B352C] border-r-4 border-b-4 py-2.5 px-5 rounded-[10px] w-68">
                         <h4 class="text-[#5B352C] text-xl">Jildan Adam Zein</h4>
                         <h5 class="text-neutral-700">Koordinator DPM FILKOM 2025</h5>
                     </div>
-                    <hr class="w-full border-[#B48B52] border-2 rounded">
+                    <hr class="w-7/8 border-[#B48B52] border-1 rounded ml-auto">
                 </div>
             </div>
         </section>
         <!-- Foto Bareng Parlemen dan Logonya -->
-        <section class="my-20 mx-auto max-w-[80%] px-4 sm:px-6 lg:px-8">
-            <img :src="fotoParlemen" class="w-full" alt="Foto Parlemen Katha Mantrana">
+        <section class="my-20 mx-20 px-4 sm:px-6 md:px-16 lg:px-20 flex flex-col gap-10">
+            <img :src="fotoParlemen" class="w-full shadow-[0px_10px_20px_0px_rgba(0,0,0,0.25)] rounded-xl" alt="Foto Parlemen Katha Mantrana">
             <div class="flex flex-row items-center justify-center gap-10 mt-6">
                 <a href="#"><img class="w-20 h-auto transition-transform duration-100 hover:animate-spin" :src="logoKoor" alt="logo Koordinator"></a>
                 <a href="#"><img class="w-20 h-auto transition-transform duration-100 hover:animate-spin" :src="logoSekjen" alt="logo Sekertaris Jendral"></a>
@@ -92,8 +92,8 @@ import logoMedkom from '/public/images/logo_medkom.png';
             </div>
         </section>
         <!-- Kegiatan, sumpah ini brarti isinya data kan ya? -->
-        <section class="my-20 mx-auto max-w-[80%] px-4 sm:px-6 lg:px-8">
-            <h2 class="mb-15 text-4xl text-[#5B352C] font-bold text-center">Kegiatan DPM FILKOM UB</h2>
+        <section class="my-20 mx-20 px-4 sm:px-6 md:px-16 lg:px-20">
+            <h2 class="mb-15 text-4xl text-[#5B352C] font-semibold text-center">Kegiatan DPM FILKOM UB</h2>
             <div class="flex flex-row gap-4 items-center justify-around mb-10">
                 <div class=" text-neutral-900">
                     <img :src="prokerPA" alt="Program Kerja Portal Aspirasi">
